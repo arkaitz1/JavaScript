38f72073f239df3fa5c28f2370ae8bb9bcead33c
ik012108bnq <ik012108bnq@plaiaundi.net>
2017-10-31 14:07:30 +0100

    31 Diciembre
    Aplicacion a publicar

---
+++ Publicar/js/PreguntasFV.js
@@ -0,0 +1,12 @@
+// Javascript document
+var iPreguntas=4;
+var aPreguntas=[];
+var aRespuestas=[];
+aPreguntas[1] = 'Paris es la captial de Alemania';
+aRespuestas[1]='F';
+aPreguntas[2] = 'El grillo es un insecto';
+aRespuestas[2]='V';
+aPreguntas[3] = 'Es el cielo azul';
+aRespuestas[3]='V';
+aPreguntas[4] = 'Es Acces una buena aplicación';
+aRespuestas[4]='F';
