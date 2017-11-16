04a59eb6da6a9e0b812afafb6e7c3aec3d1ac91f
ik012108bnq <ik012108bnq@plaiaundi.net>
2017-11-16 15:14:15 +0100

    Examen

---
+++ Examen/Exaisca/EJ2/Preguntas.js
@@ -0,0 +1,13 @@
+// JS Document
+var iPreguntas=1;
+var aPreguntas=new Array();
+var aRespuestas=new  Array();
+var aSoluciones=new Array();
+
+aPreguntas[0]="¿Cual es el animal más rapido del planeta?";
+aRespuestas[0]=new Array("Oso","Guepardo","Tigre","Elefante");
+aSoluciones[0]="1";
+
+aPreguntas[1]="¿Cual es la capital de Francia?";
+aRespuestas[1]=new Array("Ulan Bator","Donosti","Paris","Lisboa");
+aSoluciones[1]="2";
