fc6af510ff459fb93b90f317d85126c8d350ca19
ik012108bnq <ik012108bnq@plaiaundi.net>
2018-01-30 21:41:32 +0100

    Quiniela2

--- Ajax/Quiniela/EnviarQuiniela.php
+++ Ajax/Quiniela/EnviarQuiniela.php
@@ -1,10 +1,11 @@
 <?php
-  $nombre = $_REQUEST["nombre"];
-  $apuesta = $_REQUEST["apuesta"];
-  $link = @new mysqli("localhost", "isaac", "Zakarias", "isaac");
+  $strUpdate = $_GET["str"];
+
+  $link = @new mysqli("88.2.227.247", "isaac", "Karrika2010", "isaac");
   if ($link->connect_errno) {
     die('Connect Error: ' . $link->connect_error);
   }
-  $sql = "";
-  return $link->query($sql);
+  $sql = "INSERT INTO Apuestas (Comprador, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14) VALUES $strUpdate";
+  $link->query($sql);
+  echo $link->query($sql);
  ?>
--- Ajax/Quiniela/Quiniela.php
+++ Ajax/Quiniela/Quiniela.php
@@ -4,24 +4,34 @@
     <meta charset="utf-8">
     <title>Quiniela</title>
     <script type="text/javascript">
-    function enviarDatos(){
-      var xmlhttp;
-      xmlhttp=new XMLHttpRequest();
-      xmlhttp.onreadystatechange =
-        function()
-        {
-          if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    function enviarDatos(num){
+      var strUpdate = generarString(num);
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function(){
+          if (xmlhttp.readyState==4 && xmlhttp.status==200){
              var res = xmlhttp.responseText;
+             if(res == 1)
+              document.getElementById('resultado').innerHTML = "Registro insertado correctamente"
+            else
+              document.getElementById('resultado').innerHTML = "Se ha producido algun error al introducir el registro";
           }
-        }
-      xmlhttp.open("GET","EnviarQuiniela.php");
+      }
+      xmlhttp.open("GET","EnviarQuiniela.php?str="+strUpdate);
       xmlhttp.send();
+    }
+
+    function generarString(num){
+      var strApuesta = "('"+document.getElementById('nombre').value+"'";
+      for(i=1;i<num;i=i+2)
+        strApuesta += ",'"+document.getElementById('apuesta'+i).value+"'";
+      return strApuesta+")";
     }
     </script>
   </head>
   <body>
     <?php
-      $link = @new mysqli("localhost", "isaac", "Zakarias", "isaac");
+      $link = @new mysqli("88.2.227.247", "isaac", "Karrika2010", "isaac");
+
       if ($link->connect_errno) {
         die('Connect Error: ' . $link->connect_error);
       }
@@ -30,9 +40,9 @@
         while($fila = mysqli_fetch_array($resultado)){
      ?>
       <h1>La Quiniela</h1>
-      <form class="" action="index.html" method="post">
-        <p><input type="text" name="nombre" value=""></p>
-        <p><input type="submit" name="" value="Enviar"></p>
+
+        <p><input type="text" name="nombre" id="nombre" value=""></p>
+        <p><button name="" onclick="enviarDatos(<?php echo (count($fila)-1)/2 ?>)">Enviar</button></p>
         <div style="color:red;" id="resultado"></div>
       <p><h3>Jornada: <?php echo $fila[0] ?></h3></p>
      <table border="1">
@@ -42,7 +52,7 @@
         <tr>
           <td><?php echo $fila[$i] ?></td><td><?php echo $fila[$i+1] ?></td>
           <td>
-            <select class="" name="apuesta">
+            <select class="" name="apuesta<?php echo $i ?>" id="apuesta<?php echo $i ?>">
               <option value="1">1</option>
               <option value="x">x</option>
               <option value="2">2</option>
@@ -54,6 +64,6 @@
         ?>
      </table>
 <?php } ?>
-  </form>
+
   </body>
 </html>
